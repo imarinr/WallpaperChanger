fdda4a1a7657dfd603be3fca0a559a0d008a90e5
Ivan Marin <imarinr@outlook.com>
2021-07-03 09:58:00 -0500

    It now works well. It recognizes when media is not an image.

--- apod_main.rb
+++ apod_main.rb
@@ -13,9 +13,13 @@ response = apod
 
 if response.code == 200
   result = JSON.parse(response.body.to_s)
-  puts "Downloading image to #{ENV['WALLPAPERS_FOLDER']}..."
-  Down.download(result['hdurl'], destination: ENV['WALLPAPERS_FOLDER'])
-  puts 'Finished, you\'ll see it soon'
+  if result['media_type'].downcase != 'image'
+    puts 'There are no picture today, thanks for coming!!'
+  else
+    puts "Downloading image from #{result['hdurl']}\n to #{ENV['WALLPAPERS_FOLDER']}..."
+    Down.download(result['hdurl'], destination: ENV['WALLPAPERS_FOLDER'])
+    puts 'Finished, you\'ll see it soon'
+  end
 else
   puts "Something went wrong, response is: #{response}"
 end
